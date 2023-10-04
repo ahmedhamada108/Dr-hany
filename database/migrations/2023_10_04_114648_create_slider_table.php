2ba203bd8188df463bd7ba6e480a656348aedfff
ahmedhamada108 <ah034733@gmail.com>
2023-10-04 14:47:52 +0300

    Add DB migration

---
+++ database/migrations/2023_10_04_114648_create_slider_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slider', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->string('title');
+            $table->string('sub_title');
+            $table->string('description');
+            $table->string('image_path');
+            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slider');
+    }
+};
